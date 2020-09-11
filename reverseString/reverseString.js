7cd4186650045c5b79da2fc29a788ee184f0fcd6
David <berghdavid@hotmail.com>
2020-09-11 09:51:05 +0200

    Completed reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let tempStr = "";
+    for(i = 0; i < str.length; i++){
+        tempStr = str.charAt(i) + tempStr;
+    }
+    return tempStr;
 }
 
 module.exports = reverseString
