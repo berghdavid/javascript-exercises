40dcb8366b09cd843600f0ae615d4e589a05a78f
David <berghdavid@hotmail.com>
2020-09-11 10:58:54 +0200

    Completed tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,10 @@
 const ftoc = function(f) {
-  let c = 5 * (f - 32) / 9
+  let c = 5 * (f - 32) / 9;
   return Math.round(10 * c) / 10;
 }
 
 const ctof = function(c) {
-  let f = 32 + (9 * c / 5)
+  let f = 32 + (9 * c / 5);
   return Math.round(10 * f) / 10;
 }
 
